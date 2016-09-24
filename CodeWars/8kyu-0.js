b34131f72a41a3a16ea74f4879fa1639c1fa05fc
James Lemire <webdev.jameslemire@gmail.com>
2016-09-24 09:20:10 -0600

    Find sum of array without min and max

---
+++ CodeWars/8kyu-0.js
@@ -0,0 +1,21 @@
+// Sum all the numbers of the array except the highest and the lowest element (the value, not the index!).
+// (Only one element at each edge, even if there are more than one with the same value!)
+// If array is empty, null or None, or if only 1 Element exists, return 0.
+
+function sumArray(array) {
+  if( !(array) || array.length === 1 ) {
+    return 0;
+  }
+
+  array.splice(array.indexOf(Math.max.apply(null, array)), 1);
+  array.splice(array.indexOf(Math.min.apply(null, array)), 1);
+
+  var arr = array.reduce(function(total, nextNum) {
+  	return total + nextNum
+  }, 0);
+
+  return arr;
+}
+
+var arr = [1];
+sumArray(arr);
