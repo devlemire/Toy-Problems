8e52c22665fcafa09c26f4ef36f9a0c6d70015ae
James Lemire <webdev.jameslemire@gmail.com>
2016-09-24 13:15:10 -0600

    Add hex-to-decimal

---
+++ CodeWars/8kyu/8kyu-3.js
@@ -0,0 +1,6 @@
+// Write function hexToDec which converts hex number (given as a string) to decimal number.
+// https://www.codewars.com/kata/57a4d500e298a7952100035d
+
+function hexToDec(hexString){
+  return parseInt(hexString, 16);
+}
