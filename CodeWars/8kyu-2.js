97f868b3f0465ad722367a0bafe45bea441b0ab4
James Lemire <webdev.jameslemire@gmail.com>
2016-09-24 13:11:45 -0600

    Add es6 class extension

---
+++ CodeWars/8kyu-2.js
@@ -0,0 +1,7 @@
+// Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g.
+// https://www.codewars.com/kata/55a14aa4817efe41c20000bc
+class Cat extends Animal {
+  speak() {
+    return this.name + " meows.";
+  }
+}
