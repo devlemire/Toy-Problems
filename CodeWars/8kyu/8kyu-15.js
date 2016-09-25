ccf23486ab80ba1f254e9a08e52a6c4d2a529814
James Lemire <webdev.jameslemire@gmail.com>
2016-09-25 11:49:35 -0600

    Add array includes

---
+++ CodeWars/8kyu/8kyu-15.js
@@ -0,0 +1,6 @@
+// Create a method include? (JS include)that accepts a list and an item, and returns true if the item belongs to the list.
+// https://www.codewars.com/kata/545991b4cbae2a5fda000158
+
+function include(arr, item){
+  return arr.includes(item);
+}
